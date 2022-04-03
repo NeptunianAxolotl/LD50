f25671ca6171a97d139e953efc99d933e252f96e
Lex Gorecki <goreckil@retaildirections.com>
2022-04-03 19:17:47 +1000

    testing multiples of things; a bunch of placeholders

--- defs/characterPlacementDef.lua
+++ defs/characterPlacementDef.lua
@@ -3,6 +3,11 @@ local characters = {
 		name = "firefly_logger",
 		pos = {-100, 400},
 	},
+	{
+		name = "firefly_logger",
+		pos = {-300, 600},
+	},
+
 }
 
 return characters
---
+++ defs/characters/cricket.lua
@@ -0,0 +1,14 @@
+local def = {
+	radius = 42,
+	collide = true,
+	animation = "test_anim",
+	portraitNeutral = "portrait_cricket_neutral",
+	portraitHappy = "portrait_cricket_happy",
+	shadowRadius = 27,
+	mouseHit = {rx = -35, ry = -190, width = 70, height = 200},
+	collideDensity = 1,
+	radius = 16,
+	shadowRadius = 14,
+}
+
+return def
