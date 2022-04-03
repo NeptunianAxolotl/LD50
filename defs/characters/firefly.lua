ff7c208568a3ce1ba21489841fc74746d8dadb70
Lex Gorecki <goreckil@retaildirections.com>
2022-04-03 15:26:52 +1000

    flipped portraits for use on LHS of screen.  added portrait references to firefly def

--- defs/characters/firefly.lua
+++ defs/characters/firefly.lua
@@ -2,6 +2,8 @@ local def = {
 	radius = 42,
 	collide = true,
 	animation = "test_anim",
+	portraitNeutral = "firefly_blank",
+	portraitHappy = "firefly_smile",
 	shadowRadius = 27,
 	mouseHit = {rx = -35, ry = -190, width = 70, height = 200},
 	collideDensity = 1,
