b0a4c7a52f42b55e6fb500fa13c7b9c103218c2e
Kairaux <kairauxx@gmail.com>
2022-04-03 19:14:49 +1000

    Resized Stick Icon

--- resources/defs/stick_item.lua
+++ resources/defs/stick_item.lua
@@ -2,8 +2,8 @@ return {
 	name = "stick_item",
 	file = "resources/images/stick.png",
 	form = "image",
-	xScale = 0.12,
-	yScale = 0.12,
+	xScale = 0.08,
+	yScale = 0.08,
 	xOffset = 0.5,
 	yOffset = 0.5,
 }
