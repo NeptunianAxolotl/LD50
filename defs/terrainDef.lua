54e93275412e9824f97d4f167657dba0ada46544
Lex Gorecki <goreckil@retaildirections.com>
2022-04-05 00:30:02 +1000

    cleanup, map and tree location enforcement

--- defs/terrainDef.lua
+++ defs/terrainDef.lua
@@ -1,27 +1,11 @@
 local terrain = {
 	{
-		name = "tree",
-		pos = {450, 100},
-	},
-	{
-		name = "tree",
-		pos = {900, 600},
-	},
-	{
-		name = "tree",
-		pos = {500, 500},
-	},
-	{
 		name = "wood_pile",
 		pos = {-250, -200},
 		items = {
 			log_item = 3,
 			stick_item = 6
 		}
-	},
-	{
-		name = "tree",
-		pos = {800, 200},
 	},
 	{
 		name = "fire",
@@ -35,27 +19,6 @@ local terrain = {
 		name = "stick",
 		pos = {200, 200},
 	},
-	{
-		name = "tree",
-		pos = {0, 1400},
-	},
-	{
-		name = "tree",
-		pos = {-600, 200},
-	},
-	{
-		name = "tree",
-		pos = {-500, 1000},
-	},
-	
-	{
-		name = "tree",
-		pos = {100, 2500},
-	},	
-	{
-		name = "tree",
-		pos = {2500, 100},
-	},	
 }
 
 return terrain
