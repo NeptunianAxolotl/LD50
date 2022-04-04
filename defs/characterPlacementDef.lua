aa759c6052454d9aefb723c2ddf5685bbc7be704
Lex Gorecki <goreckil@retaildirections.com>
2022-04-05 08:04:47 +1000

    added crowd of brainless NPCs

--- defs/characterPlacementDef.lua
+++ defs/characterPlacementDef.lua
@@ -34,6 +34,50 @@ local characters = {
 		data = {animDir = 0},
 		pos = {-900, -1500},
 	},
+	{
+		name = "firefly_fueler0",
+		pos = {-9260, -5660},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {-10300, 460},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {-10430, 1040},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {-7530, 5310},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {4540, 2800},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {5290, 2650},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {-4470, 1010},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {-2350, -4240},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {2180, -5490},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {2420, -6360},
+	},
+	{
+		name = "firefly_fueler0",
+		pos = {1400, -6000},
+	},
 }
 
 return characters
