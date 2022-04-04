efeddf50b0ce76c622204d65d71d48763903b7fc
Lex Gorecki <goreckil@retaildirections.com>
2022-04-05 08:42:03 +1000

    removed duplicate shadowradius set

--- defs/characters/firefly.lua
+++ defs/characters/firefly.lua
@@ -6,7 +6,6 @@ local def = {
 	animationOverlay = "firefly_light_anim",
 	portraitNeutral = "portrait_firefly_neutral",
 	portraitHappy = "portrait_firefly_happy",
-	shadowRadius = 27,
 	mouseHit = {rx = -35, ry = -190, width = 70, height = 200},
 	collideDensity = 1,
 	radius = 16,
