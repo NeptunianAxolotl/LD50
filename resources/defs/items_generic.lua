4f7421dda54b83502441692beb4e662cd8194b89
GoogleFrog <googl3frog@gmail.com>
2022-04-04 13:47:24 +1000

    Added scales for generic item loader.

--- resources/defs/items_generic.lua
+++ resources/defs/items_generic.lua
@@ -2,13 +2,17 @@
 local itemNames = util.GetDefDirList("resources/images/items")
 local data = {}
 
+local scales = {
+	metal_item = 2, -- example
+}
+
 for i = 1, #itemNames do
 	data[#data + 1] = {
 		name = itemNames[i] .. "_inventory",
 		file = "resources/images/items/" .. itemNames[i] .. ".png",
 		form = "image",
-		xScale = 0.18,
-		yScale = 0.18,
+		xScale = 0.18 * (scales[itemNames[i]] or 1),
+		yScale = 0.18 * (scales[itemNames[i]] or 1),
 		xOffset = 0.5,
 		yOffset = 0.5,
 	}
@@ -16,8 +20,8 @@ for i = 1, #itemNames do
 		name = itemNames[i],
 		file = "resources/images/items/" .. itemNames[i] .. ".png",
 		form = "image",
-		xScale = 0.22,
-		yScale = 0.22,
+		xScale = 0.22 * (scales[itemNames[i]] or 1),
+		yScale = 0.22 * (scales[itemNames[i]] or 1),
 		xOffset = 0.5,
 		yOffset = 0.5,
 	}
