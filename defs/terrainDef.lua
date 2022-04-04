a04933f060441ec0b2b801ac27c098305f503e6b
Lex Gorecki <goreckil@retaildirections.com>
2022-04-05 00:48:59 +1000

    manually placed trees around main clearing

--- defs/terrainDef.lua
+++ defs/terrainDef.lua
@@ -19,6 +19,38 @@ local terrain = {
 		name = "stick",
 		pos = {200, 200},
 	},
+	{
+		name = "tree",
+		pos = {-300, 700},
+	},
+	{
+		name = "tree",
+		pos = {-1300, -150},
+	},
+	{
+		name = "tree",
+		pos = {300, -800},
+	},
+	{
+		name = "tree",
+		pos = {800, 0},
+	},
+	{
+		name = "tree",
+		pos = {-1000, 800},
+	},
+	{
+		name = "tree",
+		pos = {-1400, 400},
+	},
+	{
+		name = "tree",
+		pos = {-800, 1000},
+	},
+	{
+		name = "tree",
+		pos = {700, 1100},
+	},
 }
 
 return terrain
