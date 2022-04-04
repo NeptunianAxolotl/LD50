0ad2013142e5ee175703b0355bc4e8b6c1ef4103
GoogleFrog <googl3frog@gmail.com>
2022-04-04 16:46:21 +1000

    Digger WIP.

--- defs/features/big_digger.lua
+++ defs/features/big_digger.lua
@@ -1,3 +1,31 @@
+
+local util = require("include/util")
+
+local function InitAvoidTiles(self)
+	self.avoidTiles = {}
+	local checkPos = self.GetPos()
+	for i = 1, 8 do
+		local tileX, tileY = api.PosToTile(util.Add(util.PolarToCart(self.radius, math.pi*i/4), checkPos))
+		self.avoidTiles[tileX] = self.avoidTiles[tileX] or {}
+		self.avoidTiles[tileX][tileY] = true
+	end
+end
+
+local function FindNewDigTile(self)
+
+
+end
+
+local function DigTile(self)
+	local dug, dead = GroundHandler.DealTileDamage(self.digTile.x, self.digTile.y, 0.1*dt)
+	if dead then
+		self.digTile = false
+	end
+	if dug then
+		ChatHandler.AddMessage("make goodies")
+	end
+end
+
 local def = {
 	name = "big_digger",
 	radius = 80,
@@ -10,8 +38,21 @@ local def = {
 		behaviourDelay = 0
 	},
 	updateFunc = function (self, dt)
-		local tileX, tileY = GroundHandler.PosToTile(self.GetPos())
-		GroundHandler.DealTileDamage(tileX, tileY, 0.1*dt)
+		if not self.avoidTiles then
+			InitAvoidTiles(self)
+		end
+		
+		if not self.digTile then
+			self.searchDigTimer = util.UpdateTimer(timer, dt) 
+			if not self.searchDigTimer then
+				FindNewDigTile(self)
+				self.searchDigTimer = 1
+			end
+		end
+		
+		if self.digTile then
+			DigTile(self, dt)
+		end
 	end,
 }
 
