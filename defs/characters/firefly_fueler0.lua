8edfd8992d49f71c65e3d68212d2aa4f1d675e04
GoogleFrog <googl3frog@gmail.com>
2022-04-05 04:54:46 +1000

    Work on generic fueler0

--- defs/characters/firefly_fueler0.lua
+++ defs/characters/firefly_fueler0.lua
@@ -1,4 +1,5 @@
 
+local util = require("include/util")
 local GuyUtils = require("utilities/guyUtils")
 
 local def = {
@@ -10,7 +11,8 @@ local def = {
 	initData = {
 		items = {
 			ore_item = 0,
-		}
+		},
+		wallowingInDarkness = true,
 	},
 	behaviour = function (self, world, dt)
 		if not self.friendly then
@@ -20,10 +22,9 @@ local def = {
 		if self.moveGoalPos then
 			return
 		end
-		
-		GuyUtils.FuelFire(self, TerrainHandler.GetHomeFire(), 0.9)
+		GuyUtils.FullyGeneralHelperGuy(self)
 	end,
-		chat = {
+	chat = {
 		acceptsChat = function(self)
 			return true
 		end,
@@ -31,327 +32,6 @@ local def = {
 			return (self.friendly and "options") or "hello"
 		end,
 		scenes = {
-			options = {
-				msg = {{
-					text = "What d'you want me to do?",
-					sound = "chat_good",
-				}},			
-				replyDelay = 1,
-					replies = {
-					{
-						msg = {
-							text = "I need you to gather fuel.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_fuel",
-					},
-					{
-						msg = {
-							text = "I need you to cut down some trees.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_trees",
-					},
-					{
-						msg = {
-							text = "I need you to mine something for me.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine",
-					},
-					{
-						msg = {
-							text = "I need you to work the furnace.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_furnace",
-					},
-					{
-						msg = {
-							text = "I need you to man the workshop.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_workshop",
-					},
-					{
-						msg = {
-							text = "Keep up the good work!",
-							sound = "chat_good",
-						},
-						leadsTo = "options_nothing",
-					},
-				}
-			},
-			options_first = {
-				msg = {{
-					text = "What d'you want me to do?",
-					sound = "chat_good",
-				}},			
-				replyDelay = 1,
-					replies = {
-					{
-						msg = {
-							text = "I need you to gather fuel.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_fuel",
-					},
-					{
-						msg = {
-							text = "I need you to cut down some trees.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_trees",
-					},
-					{
-						msg = {
-							text = "I need you to mine something for me.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_first",
-					},
-					{
-						msg = {
-							text = "I need you to work the furnace.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_furnace",
-					},
-					{
-						msg = {
-							text = "I need you to man the workshop.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_workshop",
-					},
-				}
-			},
-			options_mine = {
-				msg = {{
-					text = "What'd you want me to dig for?",
-					sound = "chat_good",
-				}},		
-				replyDelay = 1,
-				replies = {
-					{
-						msg = {
-							text = "I need you to mine some coal.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_coal",
-					},
-					{
-						msg = {
-							text = "I need you to mine metal ore.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_metal",
-					},
-					{
-						msg = {
-							text = "I need you to look for rubies.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_ruby",
-					},
-					{
-						msg = {
-							text = "I need you to find some emeralds.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_emerald",
-					},
-					{
-						msg = {
-							text = "I need you to quarry some stone.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_stone",
-					},
-					{
-						msg = {
-							text = "Hold on, I need you to do something else.",
-							sound = "chat_good",
-						},
-						leadsTo = "options",
-					},
-				}
-			},
-			options_mine_first = {
-				msg = {{
-					text = "What'd you want me to dig for?",
-					sound = "chat_good",
-				}},		
-				replyDelay = 1,
-				replies = {
-					{
-						msg = {
-							text = "I need you to mine some coal.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_coal",
-					},
-					{
-						msg = {
-							text = "I need you to mine metal ore.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_metal",
-					},
-					{
-						msg = {
-							text = "I need you to look for rubies.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_ruby",
-					},
-					{
-						msg = {
-							text = "I need you to find some emeralds.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_emerald",
-					},
-					{
-						msg = {
-							text = "I need you to quarry some stone.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_mine_stone",
-					},
-					{
-						msg = {
-							text = "Hold on, I need you to do something else.",
-							sound = "chat_good",
-						},
-						leadsTo = "options_first",
-					},
-				}
-			},
-			options_mine_coal = {
-				msg = {{
-					text = "Just coal me when you need me.",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_mine"
-					self.mineType = "mine_coal"
-				end,
-				replyDelay = 1.5,
-			},
-			options_mine_metal = {
-				msg = {{
-					text = "Just coal me when you need me.",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_mine"
-					self.mineType = "mine_metal"
-				end,
-				replyDelay = 1.5,
-			},
-			options_mine_ruby = {
-				msg = {{
-					text = "...I don't have a good pun for this one, actually.  Just...gonna go do the thing.",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_mine"
-					self.mineType = "mine_ruby"
-				end,
-				replyDelay = 1.5,
-			},
-			options_mine_emerald = {
-				msg = {{
-					text = "Gonna get me some green.",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_mine"
-					self.mineType = "mine_emerald"
-				end,
-				replyDelay = 1.5,
-			},
-			options_mine_stone = {
-				msg = {{
-					text = "Sure, don't quarry about it.",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_mine"
-					self.mineType = "mine_stone"
-				end,
-				replyDelay = 1.5,
-				},
-			options_fuel = {
-				msg = {{
-					text = "Got me all fired up!",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_fuel"
-					self.mineType = "mine_none"
-				end,
-				replyDelay = 1.5,
-			},
-			options_trees = {
-				msg = {{
-					text = "Chop chop!",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_trees"
-					self.mineType = "mine_none"
-				end,
-				replyDelay = 1.5,
-			},
-			options_furnace = {
-				msg = {{
-					text = "Turnin' up the heat!",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_furnace"
-					self.mineType = "mine_none"
-				end,
-				replyDelay = 1.5,
-			},
-			options_workshop = {
-				msg = {{
-					text = "I'm workin' on it!",
-					sound = "chat_good",
-				}},		
-				onSceneFunc = function (self, player)
-					-- Called with the scene is opened.
-					--ChatHandler.AddMessage("SCENE FUNC")
-					self.jobType = "job_workshop"
-					self.mineType = "mine_none"
-				end,
-				replyDelay = 1.5,
-				},
-			options_nothing = {
-				msg = {{
-					text = "Yessir!",
-					sound = "chat_good",
-				}},			
-				replyDelay = 1.5,
-			},
 			hello = {
 				msg = {{
 					text = "What do you want?",
@@ -401,6 +81,7 @@ local def = {
 					-- Called with the scene is opened.
 					--ChatHandler.AddMessage("SCENE FUNC")
 					self.friendly = true
+					self.wallowingInDarkness = false
 				end,
 				replyDelay = 4,
 					replies = {
@@ -444,5 +125,7 @@ local def = {
 		},
 	}
 }
+
+def.chat.scenes = util.CopyTable(GuyUtils.generalHelperTable, true, def.chat.scenes)
 
 return def
