e4eb262e33dd7d0a44521469b978364aef392666
GoogleFrog <googl3frog@gmail.com>
2022-04-06 20:23:50 +1000

    IEIEIEIEIE

--- defs/items/stick_bundle_item.lua
+++ defs/items/stick_bundle_item.lua
@@ -1,7 +1,7 @@
 local def = {
 	image = "stick_bundle_item_inventory",
 	dropAs = "stick",
-	disc = "An extremely flammable bundle of sticks.",
+	desc = "An extremely flammable bundle of sticks.",
 	dropMult = Global.STICK_BUNDLE,
 	burnValue = Global.STICK_FUEL * Global.STICK_BUNDLE * Global.RESOURCE_FUEL_MULT,
 	boostValue = Global.STICK_FUEL_BOOST * Global.STICK_BUNDLE * Global.RESOURCE_BOOST_FUEL_MULT,
